771ba4bfb33308094b7c66e437398495b31d30dc
MehtabAlamKhan <kmehtabalam19@gmail.com>
2021-01-13 20:37:31 +0530

    time

--- src/Components/SortingVisualizer.jsx
+++ src/Components/SortingVisualizer.jsx
@@ -2,7 +2,7 @@ import "./styles/SortingVisualizer.css";
 import React, { useState, useEffect } from "react";
 import * as SortingAlgorithms from "./SortingAlogorithms.js";
 
-const TIMER = 1;
+const TIMER = 10;
 var timer;
 
 function SortingVisualizer() {
